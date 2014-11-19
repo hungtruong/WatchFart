9ce3789355c43c6ac41ff87853562f0b84dd0d16
Hung Truong <hung@hung-truong.com>
2014-11-18 21:34:39 -0500

    Initial Commit

---
+++ WatchFart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchFart
+//
+//  Created by Hung Truong on 11/18/14.
+//  Copyright (c) 2014 Hung Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WatchFart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WatchFart
+//
+//  Created by Hung Truong on 11/18/14.
+//  Copyright (c) 2014 Hung Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
